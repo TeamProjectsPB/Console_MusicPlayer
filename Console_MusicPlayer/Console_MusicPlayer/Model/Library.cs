35209464175c334badeda9505b14c2820e2855eb
MARC1N <Marcin@DESKTOP-B3AQV4A>
2016-10-12 00:27:07 +0200

    Wczytano taglib. Dodane klasy Playlist oraz Library.
    TODO:
    Dorobić w library odczyt i zapis pliku w formacie xml.

---
+++ Console_MusicPlayer/Console_MusicPlayer/Model/Library.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using File = TagLib.File;
+
+namespace Console_MusicPlayer.Model
+{
+    class Library
+    {
+        #region Members
+        private string url;
+        private List<TagLib.File> songsInLibrary;
+        private List<Playlist> playlistsInLibrary;
+        #endregion
+        #region Properties
+        public string Url
+        {
+            get { return url; }
+            set { url = value; }
+        }
+
+        public List<File> SongsInLibrary
+        {
+            get { return songsInLibrary; }
+            set { songsInLibrary = value; }
+        }
+
+        public List<Playlist> PlaylistsInLibrary
+        {
+            get { return playlistsInLibrary; }
+            set { playlistsInLibrary = value; }
+        }
+        #endregion
+        #region Constructors
+        public Library(string url)
+        {
+            this.url = url;
+            songsInLibrary = new List<File>();
+            playlistsInLibrary = new List<Playlist>();
+        }
+        #endregion
+
+        #region PrivateMethods
+
+        /*private void ReadPlaylist()
+        {
+            
+        }*/
+        #endregion
+        #region Methods
+
+        public void GetSongs()
+        {
+            List<string> songsUrl = new List<string>(Directory.EnumerateFiles(url, "*.*").Where(s => s.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase) || s.ToLower().EndsWith(".wav", StringComparison.OrdinalIgnoreCase)));
+            for (int i = 0; i < songsUrl.Count; i++)
+            {
+                TagLib.File newSong = TagLib.File.Create(songsUrl.ElementAt(i));
+                songsInLibrary.Add(newSong);
+            }
+        }
+
+        /*public void GetPlaylists()
+        {
+            List<string> playlistsUrls = new List<string>(Directory.EnumerateFiles(url, "*.*").Where(s => s.EndsWith(".playlist", StringComparison.OrdinalIgnoreCase)));
+
+
+        }*/
+
+
+        #endregion
+    }
+}
---
+++ Console_MusicPlayer/Console_MusicPlayer/Model/Playlist.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagLib;
+
+namespace Console_MusicPlayer.Model
+{
+    class Playlist
+    {
+        #region Members
+        private string name;
+        private List<TagLib.File> tracks;
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public List<File> Tracks
+        {
+            get { return tracks; }
+            set { tracks = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+        public Playlist(string name)
+        {
+            this.name = name;
+        }
+
+        public Playlist(List<File> tracks)
+        {
+            name = string.Empty;
+            this.tracks = tracks;
+        }
+
+        public Playlist(string name, List<File> tracks)
+        {
+            this.name = name;
+            this.tracks = tracks;
+        }
+
+        #endregion
+
+        #region Methods
+        public void AddSong(TagLib.File song)
+        {
+            tracks.Add(song);
+        }
+
+        public void DeleteSong(TagLib.File song)
+        {
+            tracks.Remove(song);
+        }
+
+        public void MoveSong(TagLib.File song, int move)
+        {
+            int songIndex = tracks.IndexOf(song);
+            tracks.Remove(song);
+            tracks.Insert(songIndex + move, song);
+        }
+        #endregion
+    }
+}
